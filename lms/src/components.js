b913ad103a4acc6ef4bea03a3b07427330603c22
Hardik <hardik.kotangale@gmail.com>
2023-03-02 18:33:16 +0530

    deleted:    src/components.js 	deleted:    src/components/CardCarousel1.js

--- lms/src/components.js
+++
@@ -1,5 +0,0 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Maps from "./components/Maps";
-import PopOver from "./components/PopOver";
-export {Header,Footer,Maps,PopOver}
